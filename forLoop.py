19de407b0154a8eb93294c55a367e78cca853d81
ElecTro-edd <ey74@kent.ac.uk>
2024-10-28 10:50:00 +0000

    part1

--- forLoop.py
+++ forLoop.py
@@ -1,1 +1,7 @@
 ### Part One -- your code goes here. 
+def main():
+    for num in [1,2,3,4,5,6,7,8,9,10]:
+        square = num ** 2
+        print(f"the square of  {num} is {square}")
+
+main()        
